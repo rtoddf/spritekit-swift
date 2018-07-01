ebb6c42cbea4ea3b397942509198129984bac93e
Tod Fleeman <rtoddf@me.com>
2018-07-01 13:43:21 -0400

    continuing with first project

--- SceneKitIntro01/SceneKitIntro01/GameViewController.swift
+++ SceneKitIntro01/SceneKitIntro01/GameViewController.swift
@@ -1,14 +1,16 @@
 import UIKit
 import SceneKit
 
-var scnView:SCNView!
-var scnScene:SCNScene!
-var cameraNode:SCNNode!
-
 class GameViewController: UIViewController {
+    var scnView:SCNView!
+    var scnScene:SCNScene!
+    var cameraNode:SCNNode!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor(white: 0.3, alpha: 1.0)
+        
         setupView()
         setupScene()
         setupCamera()
@@ -25,12 +27,14 @@ class GameViewController: UIViewController {
     
     func setupView() {
         scnView = self.view as! SCNView
+        scnView.allowsCameraControl = true
+        scnView.autoenablesDefaultLighting = true
     }
     
     func setupScene() {
         scnScene = SCNScene()
         scnView.scene = scnScene
-        scnScene.background.contents = "courseArt.scnassets/background.png"
+//        scnScene.background.contents = "courseArt.scnassets/background.png"
     }
     
     func setupCamera() {
@@ -43,15 +47,27 @@ class GameViewController: UIViewController {
     func spawnShape() {
         var geometry:SCNGeometry
         switch ShapeType.random() {
-        default:
+        case .cone:
             geometry = SCNCone(topRadius: 0.25, bottomRadius: 0.5, height: 1)
-            let geometryNode = SCNNode(geometry: geometry)
-            scnScene.rootNode.addChildNode(geometryNode)
+        case .capsule:
+            geometry = SCNCapsule(capRadius: 0.3, height: 2.5)
+        case .tube:
+            geometry = SCNTube(innerRadius: 0.25, outerRadius: 0.5, height: 1.0)
+        case .sphere:
+            geometry = SCNSphere(radius: 0.5)
+        case .torus:
+            geometry = SCNTorus(ringRadius: 0.5, pipeRadius: 0.5)
+        case .pyramid:
+            geometry = SCNPyramid(width: 1.0, height: 1.0, length: 1.0)
+        case .cylinder:
+            geometry = SCNCylinder(radius: 0.3, height: 2.5)
+        case .box:
+            geometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
         }
+        let geometryNode = SCNNode(geometry: geometry)
+        scnScene.rootNode.addChildNode(geometryNode)
     }
 }
-
-
 
 
 
